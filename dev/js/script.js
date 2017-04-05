a436a98de8dd10d71b38acac712b7c19363c2399
aalbright <Aaron@Aarons-MacBook.local>
2017-04-05 13:06:35 -0400

    More Charts
    Added pie chart and reworked bar chart.

--- dev/js/script.js
+++ dev/js/script.js
@@ -1,6 +1,5 @@
 var $ = jQuery = require('jquery');
 var d3 = require('./d3.min.js');
-var smoothState = require('./smoothState.min.js');
 require('./bootstrap.min.js');
 var Handlebars = require('handlebars');
 
@@ -24,7 +23,7 @@ function handles(){
   }
 }
 
-var warnFilename = '';
+var warnFilename;
 
 // shows content
 $('#icon-content').on('click', 'span', function() {
@@ -37,87 +36,158 @@ $('#icon-content').on('click', 'span', function() {
     // $('#icon-content').hide();
     // $('nav').hide();
     $('<div id="chart"></div>').insertAfter('.why');
+    $('<div id="chart2"></div>').insertAfter('#chart');
     $('.dest').show();
+    pieInit();
+    chartInit();
   });
 
 // hides content
 $('.dest').on('click', 'a', function() {
   // $('#icon-content').show();
   $('#chart').remove();
+  $('#chart2').remove();
   // $('nav').show();
   // $('.dest').hide();
   $('.icondest').removeClass(warnFilename);
 })
 
-
 });
 
 
-// builds D3 chart
-function chartInit(){
+// builds D3 pie chart
+function pieInit(){
+  var piedata = [];
+
+  d3.json('data/data.json', function(data) {
+
+      for (key in data) {
+        piedata.push(data[key])
+      }
+
+      var width = 300,
+          height = 300,
+          radius = Math.min(width, height) / 2,
+          colors = d3.scale.linear()
+                  .domain([0, 10])
+                  .range(['#00C851', '#ff3d00']);
+
+      var pie = d3.layout.pie()
+          .value(function(d) {
+            return d.value;
+          })
+
+      var arc = d3.svg.arc()
+      .innerRadius(radius - 100)
+      .outerRadius(radius - 20);
+
+      var myChart = d3.select('#chart').append('svg')
+          .attr('width', width)
+          .attr('height', height)
+          .append('g')
+          .attr('transform', 'translate('+(width-radius)+','+(height-radius)+')')
+          .selectAll('path').data(pie(piedata))
+          .enter().append('g')
+              .attr('class','slice')
+
+      var slices = d3.selectAll('g.slice')
+              .append('path')
+              .attr ('fill', function(d, i){
+                return colors(i);
+              })
+              .attr('d', arc)
+
+      var text = d3.selectAll('g.slice')
+            .append('text')
+            .text(function(d, i) {
+            return d.data.value + "%"
+            })
+            .attr('text-anchor', 'middle')
+            .attr('fill','white')
+            .attr('transform', function(d){
+              d.innerRadius = 0;
+              d.outerRadius = radius;
+              return 'translate('+ arc.centroid(d)+')'
+            })
+  });
+}
+
+function chartInit() {
   var bardata = [];
 
-  for (var i=0; i < 15; i++) {
-      bardata.push(Math.round(Math.random()*100)+10)
-  };
-
-  bardata.sort(function compareNumbers(a,b) {
-      return b -a;
-  });
-
-  var width = 600,
-      height = 300
-
-  var tempColor;
-
-  var colors = d3.scale.linear()
-  .domain([0, bardata.length])
-  .range(['#ff3d00', '#00C851'])
+  var w = 500,
+      barH = 30;
 
   var x = d3.scale.linear()
-          .domain([0, d3.max(bardata)])
-          .range([0, width]);
+      .range([0, w]);
 
-  var y = d3.scale.ordinal()
-          .domain(d3.range(0, bardata.length))
-          .rangeBands([0, height], 0.2)
+  var myChart = d3.select('#chart2').append('svg')
+      .attr('width', w);
 
-    var myChart = d3.select('#chart').append('svg')
-                .attr('height', height)
-                .attr('width', width)
-                .append('g')
-                .selectAll('rect').data(bardata)
-                .enter().append('rect')
-                    .style('fill', function(d,i) {
-                        return colors(i);
-                    })
-                    .attr('height', y.rangeBand())
-                    .attr('y', function(d,i) {
-                        return y(i);
-                    })
-                    .attr('width', 0)
-                    .attr('x', 0)
+  d3.json('data/cost.json', function(error, data) {
 
-                .on('mouseover', function(d) {
+    for (j in data) {
+      bardata.push(data[j])
+    }
+    var dataYear = bardata.map(function(o){return o.year}),
+        dataCost = bardata.map(function(o){return o.cost});
 
-                    tempColor = this.style.fill;
-                    d3.select(this)
-                        .style('opacity', .5)
-                        .style('fill', 'yellow')
-                })
+    x.domain([0, d3.max(dataCost)]);
 
-                .on('mouseout', function(d) {
-                    d3.select(this)
-                        .style('opacity', 1)
-                        .style('fill', tempColor)
-                })
-    myChart.transition()
-    .attr('width', function(d) {
-    return x(d);
-    })
-    .attr('x', 0)
-    .delay(function(d, i) {
-    return i * 5;
-    })
-    .duration(500)
+    var tempColor;
+
+    var colors = d3.scale.linear()
+      .domain([0, dataCost.length])
+      .range(['#ff3d00', '#00C851']);
+
+    // var y = d3.scale.ordinal()
+    //       .domain(d3.range(0, dataCost.length))
+    //       .rangeBands([0, h], 0.2);
+
+    myChart.attr('height', barH * dataCost.length);
+
+    var bar = myChart.selectAll('g')
+        .data(dataCost)
+        .enter().append('g')
+        .attr("transform", function(d, i) { return "translate(0," + i * barH + ")"; });
+
+      bar.append('rect')
+          .style('fill', function(d,i) {
+              return colors(i);
+          })
+          .attr('height', barH - 1)
+          // .attr('y', function(d,i) {
+          //     return y(i);
+          // })
+          .attr('width', function(d) { return x(d); })
+          // .attr('x', 0)
+      bar.append("text")
+        .attr("x", function(d) { return x(d) - 3; })
+        .attr("y", barH / 2)
+        .attr("dy", ".35em")
+        .text(function(d) { return "$"+d; });
+
+      bar.on('mouseover', function(d) {
+
+          tempColor = this.style.fill;
+          d3.select(this)
+              .style('opacity', .5)
+      });
+
+      bar.on('mouseout', function(d) {
+          d3.select(this)
+              .style('opacity', 1)
+              .style('fill', tempColor)
+      });
+    // bar.transition()
+    // .attr('width', function(d) {
+    // return x(d);
+    // })
+    // .attr('x', 0)
+    // .delay(function(d, i) {
+    // return i * 5;
+    // })
+    // .duration(500)
+
+    });
 }
